1aff1db18617fd783bd380fba4bc7ab059bc7d45
d-smith <doug.smith.mail@gmail.com>
2021-06-15 14:08:47 -0700

    added api, python to configure doomed delivery, log queries

--- readme.md
+++ readme.md
@@ -3,8 +3,9 @@
 Parts:
 
 * sls api to post JSON to an endpoint
+* queue subscription and delivery config
 
 Call it:
 
 ```
-curl -X POST -H 'x-api-key: xxx' -d '{}' https://xxx.execute-api.us-east-1.amazonaws.com/dev/wp/t1
+curl -X POST -H 'x-api-key: xxx' -d '{"foo":true}' https://xxx.execute-api.us-east-1.amazonaws.com/dev/wp/t1
--- wp.js
+++ wp.js
@@ -1,9 +1,55 @@
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+const { v4: uuidv4 } = require('uuid');
+
+let topic_arn = process.env.TOPIC_ARN;
+
+
 const wrapPub = async (event) => {
     console.log(event);
-    return {
-        statusCode: 200,
-        body: "yo"
-    };
+
+    let cloudEvent = {
+        id: uuidv4(),
+        source: 'your mama',
+        specversion: '1.0',
+        type: event.pathParameters.topic,
+        data: JSON.parse(event.body),
+        time: new Date().toISOString()
+    }
+
+    console.log(cloudEvent);
+
+    console.log(topic_arn);
+
+    try {
+        let snsParams = {
+            Message: JSON.stringify(cloudEvent),
+            TopicArn: topic_arn
+        };
+
+        let response = await sns.publish(snsParams).promise();
+        console.log(response);
+
+        pubContext = {
+            PublishContext: {
+                EventId: cloudEvent.id,
+                MessageId: response.MessageId
+            }
+        };
+
+        console.log(JSON.stringify(pubContext));
+
+        return {
+            statusCode: 200,
+            body: "ok"
+        };
+    } catch(err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: "not ok"
+        };
+    }
 };
 
 
